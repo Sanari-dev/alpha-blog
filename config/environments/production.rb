4505d7fe481d7ce86e1535f9d98cefb2444e7d6e
Sanari <sanari.dev@gmail.com>
2023-09-24 08:50:05 +0700

    Remove config host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "alphablog1-waweoyz3.b4a.run/"
+  # config.hosts << ""
   # Code is not reloaded between requests.
   config.cache_classes = true
 
