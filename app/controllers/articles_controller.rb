77c08241a3d6b9d7898b15ea99c512c332a5919e
Sanari <sanari.dev@gmail.com>
2023-09-30 11:09:31 +0700

    CRUD articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,53 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all  
   end
   
   def show
-    @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end  
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created succesfully."
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated succesfully."
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @article.destroy
+      flash[:notice] = "Article was deleted succesfully."
+    end
+    redirect_to articles_path, status: :see_other
+  end
+
+  private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 end
